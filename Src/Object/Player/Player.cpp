3f285213116ed38cd5f0944395bd4633de6d2822
ASOST\2416043 <y.yuuto68@gmail.com>
2025-05-09 12:53:28 +0900

    プレイヤーのジャンプ動作

--- Src/Object/Player/Player.cpp
+++ Src/Object/Player/Player.cpp
@@ -24,12 +24,21 @@ void Player::GameInit()
 	player_.radius_ = RADIUS;
 	player_.hp_ = HP_MAX;
 	player_.speed_ = MOVE_POW;
+	verticalAcceleration_ = 0;
+	jumpPower_ = 0;
+	isJump_ = false;
+	isJumpKey_ = false;
+	inputJumpKeyCounter_ = 0;
+	gravity_ = GRAVITY;
 }
 
 void Player::Update()
 {
 	Move();
+	ProcessJump();
+	UpdatePositionY();
 
+	CollisionStage();
 }
 
 void Player::Draw()
@@ -55,3 +64,62 @@ void Player::Move()
 		player_.pos_.x -= player_.speed_;
 	}
 }
+
+
+void Player::UpdatePositionY(void)
+{
+	player_.pos_.y += verticalAcceleration_;
+}
+
+void Player::ProcessJump(void)
+{
+	if (InputManager::GetInstance().IsNew(KEY_INPUT_J)) {
+		isJump_ = true;
+		isJumpKey_ = true;
+	}
+
+	if (InputManager::GetInstance().IsNew(KEY_INPUT_J) 
+		&&inputJumpKeyCounter_<INPUT_JUMPKEY_FRAME
+		&& isJumpKey_) {
+		inputJumpKeyCounter_++;
+		jumpPower_ = jumpPower_ + (MAX_JUMP_POWER / static_cast<float>(INPUT_JUMPKEY_FRAME));
+
+		Jump();
+	}
+	if (InputManager::GetInstance().IsTrgUp(KEY_INPUT_J)) {
+		inputJumpKeyCounter_ = INPUT_JUMPKEY_FRAME;
+	}
+}
+
+
+void Player::Jump(void)
+{
+	//Y軸加速度にジャンプ力を加える
+	verticalAcceleration_ += jumpPower_;
+	//Y軸加速度が最大ジャンプ力を超えたら最大ジャンプ量に設定
+	verticalAcceleration_ = (verticalAcceleration_ < MAX_JUMP_POWER) ? MAX_JUMP_POWER : verticalAcceleration_;
+}
+
+void Player::Gravity(void)
+{
+	//Y軸加速度に重力を加える
+	verticalAcceleration_ += gravity_;
+}
+
+void Player::CollisionStage(void)
+{
+	if (player_.pos_.y <= 500) {
+		Gravity();
+	}
+	else {
+		player_.pos_.y = 500;
+		verticalAcceleration_ = 0;
+		isJump_ = false;
+		isJumpKey_ = false;
+		jumpPower_ = 0;
+		inputJumpKeyCounter_ = 0;
+
+	}
+
+}
+
--- Src/Object/Player/Player.h
+++ Src/Object/Player/Player.h
@@ -7,8 +7,15 @@ public:
 	static constexpr int SIZE_X = 64;
 	static constexpr int SIZE_Y = 96;
 	static constexpr float RADIUS = 64.0f;
+
 	static constexpr int HP_MAX = 100;
-	static constexpr float MOVE_POW = 10.0f;
+
+	static constexpr float MOVE_POW = 10.0f;		//移動量
+
+	static constexpr float MAX_JUMP_POWER = -50.0f;	//最大ジャンプ力
+	static constexpr int INPUT_JUMPKEY_FRAME = 6;	//ジャンプ入力受付フレーム数
+
+	static constexpr float GRAVITY = 9.8f;			//重力
 
 	Player();
 	~Player();
@@ -33,8 +40,31 @@ private:
 	//動作
 	void Move();
 
+	//重力
+	void Gravity(void);
 
+	//重力
+	float gravity_;
 
+	//Y座標の変更
+	void UpdatePositionY(void);
+
+	//ジャンプ
+	void Jump(void);
+
+	//ジャンプ発動処理
+	void ProcessJump(void);
+
+	bool isJump_;		//true=ジャンプ中/false=非ジャンプ
+	bool isJumpKey_;		//true=ジャンプキーが押されている/false=非ジャンプキーが押されている
+
+	float jumpPower_;		//ジャンプパワー
+
+	int inputJumpKeyCounter_;		//ジャンプの入力時間カウンター
+
+	float verticalAcceleration_;			//縦方向の加速度
+
+	void CollisionStage(void);
 };
 
 
