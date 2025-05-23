1bd30a19119ee7e2af1656909050e63ad2b32c77
2416081 <2416081@PC6512125>
2025-05-23 16:37:58 +0900

    スクロール正常にした

--- Src/Scene/GameScene.cpp
+++ Src/Scene/GameScene.cpp
@@ -56,13 +56,15 @@ void GameScene::Update(void)
 
 	if (player_->GetPlayer().disppos_.x > SceneManager::MAIN_SCREEN_SIZE_X / 7 * 4) {
 		Camera::GetInstance().Follow(Camera::dir::X, player_->GetPlayer().speed_);
+		if (player_->IsEvasion())Camera::GetInstance().Follow(Camera::dir::X, Player::EVASION_LENGTH);
 	}
 	if (player_->GetPlayer().disppos_.x < SceneManager::MAIN_SCREEN_SIZE_X / 7 * 3) {
 		Camera::GetInstance().Follow(Camera::dir::X, -(player_->GetPlayer().speed_));
+		if (player_->IsEvasion())Camera::GetInstance().Follow(Camera::dir::X, -Player::EVASION_LENGTH);
 	}
 
 
-	
+
 	if (player_->IsEvasion()) {
 		SceneManager::GetInstance().ZoomPos(player_->GetPlayer().disppos_);
 		SceneManager::GetInstance().ZoomScale(1.4f);
