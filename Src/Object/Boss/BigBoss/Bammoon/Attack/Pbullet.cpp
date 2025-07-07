9a6d4c31dbeac272b99edc85f58f5ca7a3b7053c
DESKTOP-S2QG9IR\PC_User <hal.20060214@icloud.com>
2025-07-08 02:28:23 +0900

    Pbullet続き

--- Src/Object/Boss/BigBoss/Bammoon/Attack/Pbullet.cpp
+++ Src/Object/Boss/BigBoss/Bammoon/Attack/Pbullet.cpp
@@ -1,5 +1,10 @@
 #include "Pbullet.h"
 
+#include<string>
+
+#include"../../../../../Manager/Camera.h"
+#include"../../../../../Utility/AsoUtility.h"
+
 Pbullet::Pbullet() {
 }
 
@@ -10,31 +15,78 @@ void Pbullet::Init(const Vector2F* pos)
 {
 	AttackBase::Init(pos);
 
+	obj_.resize(NUM);
+	move_.resize(NUM);
+	animeCou_.resize(NUM);
+	for (int i = 0; i < NUM; i++) {
+		obj_[i].isAlive_ = false;
+		obj_[i].pos_ = {};
+
+		move_[i] = {};
+		animeCou_[i] = 0;
+		hit_[i] = false;
+	}
+
 	end_ = false;
 
-
+	for (int i = 0; i < PBULLET_ANIME_NUM; i++) {
+		std::string path = "Data/Image/Boss/Bammmoon/AttackEffect/Pbullet" + std::to_string(i + 1) + ".png";
+		pBulletImg_[i] = LoadGraph(path.c_str());
+		if (pBulletImg_[i] == -1) {
+			return;
+		}
+	}
+	for (int i = 0; i < HIT_ANIME_NUM; i++) {
+		std::string path = "Data/Image/Boss/Bammmoon/AttackEffect/Hit/hits" + std::to_string(i + 1) + ".png";
+		hitImg_[i] = LoadGraph(path.c_str());
+		if (hitImg_[i] == -1) {
+			return;
+		}
+	}
 }
 
 void Pbullet::Update(void) 
 {
+	int i = -1;
 	for (auto& p : obj_) {
+		i++;
 		if (!p.isAlive_)continue;
 
+		p.pos_ += move_[i];
+
+		p.disppos_ = p.pos_ - Camera::GetInstance().GetPos();
 
 
-
+		animeCou_[i]++;
+		if (animeCou_[i] < PBULLET_ANIME_NUM)animeCou_[i] = 0;
 	}
 }
 
 void Pbullet::Draw(void) 
 {
+	int i = -1;
+	for (auto& p : obj_) {
+		i++;
+		if (!p.isAlive_)continue;
+
+		DrawRotaGraph(p.disppos_.x, p.disppos_.y, 1, atan2(move_[i].y, move_[i].x), pBulletImg_[i], true);
+
+
+	}
 }
 
 void Pbullet::Release(void) 
 {
+	for (int i = 0; i < HIT_ANIME_NUM; i++) {
+		DeleteGraph(hitImg_[i]);
+	}
+	for (int i = 0; i < PBULLET_ANIME_NUM; i++) {
+		DeleteGraph(pBulletImg_[i]);
+	}
+	obj_.clear();
 }
 
-void Pbullet::On(int i, Vector2F pPos) 
+void Pbullet::On(int i, Vector2F pPos)
 {
 	if (obj_.size() <= i)return;
 
@@ -43,6 +95,11 @@ void Pbullet::On(int i, Vector2F pPos)
 	Vector2F v = pPos - obj_[i].pos_;
 	float dis = sqrtf(v.x * v.x + v.y * v.y);
 	v /= dis;
+
+	float rad = RAD * (i - NUM / 2);
+
+	v.x += sinf(rad);
+	v.y += cosf(rad);
 
 	move_[i] = v * SPEED;
 
--- Src/Object/Boss/BigBoss/Bammoon/Attack/Pbullet.h
+++ Src/Object/Boss/BigBoss/Bammoon/Attack/Pbullet.h
@@ -2,15 +2,20 @@
 
 #include"../../../Tutorial/Attack/AttackBase.h"
 
+
 class Pbullet : AttackBase
 {
 public:
+	static constexpr int NUM = 10;
+
 	static constexpr int PBULLET_ANIME_NUM = 4;
 
 	static constexpr int HIT_ANIME_NUM = 5;
 
 
 	static constexpr float SPEED = 5.0f;
+
+	static constexpr float RAD = 10.0f * (DX_PI_F / 180.0f);
 
 	Pbullet();
 	~Pbullet();
@@ -29,6 +34,12 @@ private:
 	std::vector<Base> obj_;
 
 	std::vector<Vector2F> move_;
+	std::vector<int> animeCou_;
+
+	int pBulletImg_[PBULLET_ANIME_NUM];
+	int hitImg_[HIT_ANIME_NUM];
+
+	std::vector<bool>hit_;
 
 };
 
